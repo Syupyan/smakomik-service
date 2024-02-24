5d601924f2a3d49e2204734cdf2832e323802e87
syupyan13 <syupyan@gmail.com>
2024-02-24 13:03:58 +0800

    env

--- config/setup.go
+++ config/setup.go
@@ -13,23 +13,39 @@ import (
 	"gorm.io/gorm"
 )
 
+// func ConnDb() *gorm.DB {
+// 	err := godotenv.Load()
+// 	if err != nil {
+// 		log.Fatal("Error loading .env file")
+// 	}
+// 	dbHost := os.Getenv("DB_HOST")
+// 	dbPort := os.Getenv("DB_PORT")
+// 	dbUser := os.Getenv("DB_USER")
+// 	dbPass := os.Getenv("DB_PASS")
+// 	dbName := os.Getenv("DB_NAME")
+// 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbPort, dbName)
+// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// 	if err != nil {
+// 		log.Fatalf("Error connecting to database: %v", err)
+// 	}
+// 	return db
+// }
+
 func ConnDb() *gorm.DB {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbUser := os.Getenv("DB_USER")
-	dbPass := os.Getenv("DB_PASS")
-	dbName := os.Getenv("DB_NAME")
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbPort, dbName)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatalf("Error connecting to database: %v", err)
-	}
-	return db
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+    dbURI := os.Getenv("DB_URI")
+
+    // Membuat koneksi ke database menggunakan URI
+    db, err := gorm.Open(mysql.Open(dbURI), &gorm.Config{})
+    if err != nil {
+        log.Fatalf("Error connecting to database: %v", err)
+    }
+    return db
 }
+
 
 func AuthMiddlewareWithConfig(config jwtware.Config) fiber.Handler {
     return func(c *fiber.Ctx) error {
